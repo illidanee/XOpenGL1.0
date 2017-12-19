2b2867fc7d3733163bbd62714d5a73479c1fa25f
illidan <smile@illidan.org>
2017-12-19 16:24:16 +0800

    OpenGL-Win32-FullScreen

---
+++ OpenGL-Win32-FullScreen/Main.cpp
@@ -0,0 +1,105 @@
+#include <tchar.h>
+#include <windows.h>
+
+#include "XGLContent.h"
+
+
+/****************************************************************************************************************
+*    Date    : 2017/12/19 16:13
+*
+*    Author  : Smile
+*    Contact : smile@illidan.org
+*
+*    Brief   : 设置窗口为全屏
+*
+****************************************************************************************************************/
+void ChangleToFullScreen(int w, int h)
+{
+	DEVMODE dmSettings = { 0 };
+	if (!EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &dmSettings))
+		return;
+
+	dmSettings.dmPelsWidth = w;
+	dmSettings.dmPelsHeight = h;
+	dmSettings.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT | DM_BITSPERPEL;
+
+	int res = ChangeDisplaySettings(&dmSettings, CDS_FULLSCREEN);
+	if (res != DISP_CHANGE_SUCCESSFUL)
+		PostQuitMessage(0);
+}
+
+LRESULT CALLBACK WindowProc(
+	_In_ HWND   hwnd,
+	_In_ UINT   uMsg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;		
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int __stdcall WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	WNDCLASSEX wc;
+	memset(&wc, 0, sizeof(wc));
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = 0;
+	wc.hCursor = 0;
+	wc.hIcon = 0;
+	wc.hIconSm = 0;
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WindowProc;
+	wc.lpszClassName = _T("illidan");
+	wc.lpszMenuName = 0;
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC | CS_DBLCLKS;
+
+	RegisterClassEx(&wc);
+
+	int screenW = GetSystemMetrics(SM_CXSCREEN);
+	int screenH = GetSystemMetrics(SM_CYSCREEN);
+
+	/*
+	使用全屏窗口时:
+	1 - 创建窗口风格需要为：WS_POPUP | WS_VISIBLE。
+	2 - 创建窗口大小需要显示设置，不能为CW_USEDEFAULT。
+	*/
+
+	HWND hWnd = CreateWindowEx(0, _T("illidan"), _T("Window"), WS_POPUP | WS_VISIBLE, 0, 0, screenW, screenH, 0, 0, hInstance, 0);
+	
+	ChangleToFullScreen(screenW, screenH);
+
+	UpdateWindow(hWnd);
+	ShowWindow(hWnd, SW_SHOW);
+	
+	Smile::XGLContent _glContent;
+	_glContent.Begin(hWnd);
+
+	MSG msg = { 0 };
+
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		{
+			glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			_glContent.SwapBuffer();
+			Sleep(1);
+		}
+	}
+
+	_glContent.End();
+
+	return 0;
+}
---
+++ OpenGL-Win32-FullScreen/XGLContent.cpp
@@ -0,0 +1,49 @@
+#include "XGLContent.h"
+
+namespace Smile
+{
+	XGLContent::XGLContent()
+	{
+		_hWnd = 0;
+		_hDC = 0;
+		_hGLRC = 0;
+	}
+
+	void XGLContent::Begin(HWND hWnd)
+	{
+		_hWnd = hWnd;
+		_hDC = GetDC(_hWnd);
+
+		PIXELFORMATDESCRIPTOR pfd;
+		memset(&pfd, 0, sizeof(pfd));
+		pfd.nSize = sizeof(pfd);
+		pfd.nVersion = 1;
+		pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+		pfd.iPixelType = PFD_TYPE_RGBA;
+		pfd.cColorBits = 32;
+		pfd.cDepthBits = 24;
+		pfd.cStencilBits = 8;
+		pfd.iLayerType = PFD_MAIN_PLANE;
+
+		int iPixelFormat = ChoosePixelFormat(_hDC, &pfd);
+		SetPixelFormat(_hDC, iPixelFormat, &pfd);
+
+		_hGLRC = wglCreateContext(_hDC);
+		wglMakeCurrent(_hDC, _hGLRC);
+	}
+
+	void XGLContent::SwapBuffer()
+	{
+		SwapBuffers(_hDC);
+	}
+
+	void XGLContent::End()
+	{
+		wglMakeCurrent(0, 0);
+		wglDeleteContext(_hGLRC);
+		_hGLRC = 0;
+		ReleaseDC(_hWnd, _hDC);
+		_hDC = 0;
+		_hWnd = 0;
+	}
+}
