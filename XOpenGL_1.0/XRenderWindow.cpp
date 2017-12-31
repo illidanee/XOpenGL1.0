dcc053c3cc6ec105cedbba6754edd24c42f77ea1
illidan <smile@illidan.org>
2017-12-31 15:50:23 +0800

    2.2-实例：简单绘制图片

--- XOpenGL_1.0/XRenderWindow.cpp
+++ XOpenGL_1.0/XRenderWindow.cpp
@@ -2,20 +2,80 @@
 
 namespace Smile
 {
-	void XRenderWindow::Begin()
+	struct Vertex
 	{
+		float x, y, z;
+		float u, v;
+	};
 
+	Vertex g_cubeVertices[] =
+	{
+		{ 200,150,0.0f,	0.0f,0.0f},
+		{ 600,150,0.0f, 1.0f,0.0f},
+		{ 600,450,0.0f, 1.0f,1.0f},
+		{ 200,450,0.0f,	0.0f,1.0f}
+	};
+
+	GLuint _texture;
+
+	void DrawOnCPU()
+	{
+		//模型矩阵
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+		glBindTexture(GL_TEXTURE_2D, _texture);
+
+		//绑定数据
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &g_cubeVertices[0].x);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), &g_cubeVertices[0].u);
+
+		//绘制
+		glDrawArrays(GL_QUADS, 0, 4);
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
-	void XRenderWindow::Render()
+	void Smile::XRenderWindow::Begin()
+	{
+		char* pBuffer;
+		int w, h;
+
+		glEnable(GL_TEXTURE_2D);
+		XResource::LoadTextureFile("../Resources/1.jpg", &pBuffer, &w, &h);
+		{
+			glGenTextures(1, &_texture);
+			glBindTexture(GL_TEXTURE_2D, _texture);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, pBuffer);
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+	}
+
+	void Smile::XRenderWindow::Render()
 	{
 		//清空
-		glClearColor(0.15f, 0.25f, 0.35f, 1.0f);
+		glEnable(GL_DEPTH_TEST);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//正交投影绘制平面
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, _w, 0, _h, -100, 100);
+
+		DrawOnCPU();
 	}
 
-	void XRenderWindow::End()
+	void Smile::XRenderWindow::End()
 	{
+		glDeleteTextures(1, &_texture);
 	}
 }
 
