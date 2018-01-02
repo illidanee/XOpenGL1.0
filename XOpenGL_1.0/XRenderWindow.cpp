fc60f468d157cba4f707fab4331a25985d2dce8a
illidan <smile@illidan.org>
2018-01-02 10:04:54 +0800

    2.9-实例：低版本实现画中画。

--- XOpenGL_1.0/XRenderWindow.cpp
+++ XOpenGL_1.0/XRenderWindow.cpp
@@ -21,18 +21,6 @@ namespace Smile
 	GLuint _texture;
 	GLuint _dynamic;
 
-	GLuint _pbo[2];
-	int _DMA;
-	int _Write;
-	
-	void RandomBuffer(char* pData, int w, int h)
-	{
-		for (unsigned int i = 0; i < w * h * 4; ++i)
-		{
-			pData[i] = rand();
-		}
-	}
-
 	void DrawOnCPU(GLuint texture)
 	{
 		//Ä£ÐÍ¾ØÕó
@@ -81,19 +69,6 @@ namespace Smile
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _w, _h, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 			glBindTexture(GL_TEXTURE_2D, 0);
 		}
-
-		glGenBuffers(2, _pbo);
-
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, _pbo[0]);
-		glBufferData(GL_PIXEL_UNPACK_BUFFER, _w * _h * 4, 0, GL_STREAM_READ);
-
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, _pbo[1]);
-		glBufferData(GL_PIXEL_UNPACK_BUFFER, _w * _h * 4, 0, GL_STREAM_READ);
-
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
-
-		_DMA = 0;
-		_Write = 1;
 	}
 
 	void Smile::XRenderWindow::Render()
@@ -107,20 +82,14 @@ namespace Smile
 		glLoadIdentity();
 		glOrtho(0, _w, 0, _h, -100, 100);
 
-		//±£´æÍ¼Æ¬
+		DrawOnCPU(_texture);
+
 		glBindTexture(GL_TEXTURE_2D, _dynamic);
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, _pbo[_DMA]);
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, _w, _h, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+		glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 0, 0, _w, _h);
+		glBindTexture(GL_TEXTURE_2D, 0);
 
-		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, _pbo[_Write]);
-		void* pData = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_READ_ONLY);
-		if (pData)
-		{
-			RandomBuffer((char*)pData, _w, _h);
-		}
-		glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
-		std::swap(_DMA, _Write);
-
+		glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		DrawOnCPU(_dynamic);
 	}
 
