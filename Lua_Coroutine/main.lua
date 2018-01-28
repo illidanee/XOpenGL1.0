efb268e63843980d2f7bcc53687ee5295b52e5fa
illidan <smile@illidan.org>
2018-01-28 12:01:05 +0800

    4.16-协程参数和返回值的示例。

--- Lua_Coroutine/main.lua
+++ Lua_Coroutine/main.lua
@@ -1,23 +1,47 @@
 --定义协程体
-function f1()
-	print('------------Step 1');
-	coroutine.yield();
-	print('------------Step 2');
-	coroutine.yield();
-	print('------------Step 3');
-	coroutine.yield();
-	print('------------Step 4');
-	coroutine.yield();
-	print('------------Step 5');
+function f1(a, b, c)
+	print('------------Step 0', a, b, c);
+	
+	a = a + 1;
+	b = b + 1;
+	c = c + 1;
+	print('------------Step 1', a, b, c);
+	coroutine.yield(a, b, c);
+	
+	
+	a = a + 1;
+	b = b + 1;
+	c = c + 1;
+	print('------------Step 2', a, b, c);
+	coroutine.yield(a, b, c);
+
+	
+	a = a + 1;
+	b = b + 1;
+	c = c + 1;
+	print('------------Step 3', a, b, c);
+	coroutine.yield(a, b, c);
+
+	
+	a = a + 1;
+	b = b + 1;
+	c = c + 1;
+	print('------------Step 4', a, b, c);
+	coroutine.yield(a, b, c);
+	
+	a = a + 1;
+	b = b + 1;
+	c = c + 1;
+	print('------------Step 5', a, b, c);
 end
 
 --运行协程
 co = coroutine.create(f1);
-coroutine.resume(co);
+coroutine.resume(co, 0, 100, 1000);
 
 --恢复执行
 function cc()
-	coroutine.resume(co);
+	print(coroutine.resume(co, a, b, c));
 end
 
 --查看状态
